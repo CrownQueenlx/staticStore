0d8811170710107cba832ebdfd2312545dded1dc
Brianne W <brianne4god@gmail.com>
2023-04-30 21:30:57 -0400

    smartDraft

---
+++ myStore.js
@@ -0,0 +1,7 @@
+
+//this is an array of photos
+let Photo = Image
+Photo = ["D:\Programs (save space)\ElevenFiftyProjects\codingFoundations\assignments\StaticStoreCustom\assets\victor-rutka-4FujjkcI40g-unsplash.jpg",
+"D:\Programs (save space)\ElevenFiftyProjects\codingFoundations\assignments\StaticStoreCustom\assets\alana-harris-C63YZ33DdvY-unsplash.jpg",
+"D:\Programs (save space)\ElevenFiftyProjects\codingFoundations\assignments\StaticStoreCustom\assets\demi-deherrera-L-sm1B4L1Ns-unsplash.jpg",
+"D:\Programs (save space)\ElevenFiftyProjects\codingFoundations\assignments\StaticStoreCustom\assets\lex-sirikiat-VkUP6wWqSvw-unsplash.jpg"].src
